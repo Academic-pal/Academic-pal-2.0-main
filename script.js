55373f2e8d9f4601fc2e9f8ffb83c4cdac644085
Your Name <your.email@example.com>
2024-06-16 13:20:52 +0530

    hari

--- script.js
+++ script.js
@@ -185,3 +185,14 @@ document.querySelectorAll('.bottom-icons a').forEach(anchor => {
     });
   });
 });
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(error) {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
