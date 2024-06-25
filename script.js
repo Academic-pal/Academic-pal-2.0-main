2c82d0e6d629ad81501388c79c74105f540ca112
Your Name <your.email@example.com>
2024-06-25 18:13:59 +0530

    hari

--- script.js
+++ script.js
@@ -187,16 +187,6 @@ document.querySelectorAll('.bottom-icons a').forEach(anchor => {
 });
 
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(error) {
-      console.log('ServiceWorker registration failed: ', error);
-    });
-  });
-}
-
 
 
 
